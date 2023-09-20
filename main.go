b75884bd8de1c87c43a07a3e98922855801e0ca0
qiumozhou_hg <qiumozhou@iswclub.cn>
2023-09-20 14:04:07 +0800

    endless实现服务热重启

--- main.go
+++ main.go
@@ -2,23 +2,39 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"log"
+	"syscall"
 
+	"github.com/fvbock/endless"
 	"github.com/qmz/go-gin-blog/pkg/setting"
 	"github.com/qmz/go-gin-blog/routers"
 )
 
 func main() {
 
-	routersInit := routers.InitRouter()
+	// routersInit := routers.InitRouter()
 
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:        routersInit,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	// s := &http.Server{
+	// 	Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+	// 	Handler:        routersInit,
+	// 	ReadTimeout:    setting.ReadTimeout,
+	// 	WriteTimeout:   setting.WriteTimeout,
+	// 	MaxHeaderBytes: 1 << 20,
+	// }
+
+	// s.ListenAndServe()
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+
+	server := endless.NewServer(endPoint, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
 
-	s.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
 }
