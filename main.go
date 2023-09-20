6cf52bdbf1d93e0e866277fccec97e92d8fb6605
qiumozhou_hg <qiumozhou@iswclub.cn>
2023-09-20 16:31:35 +0800

    添加swagger

--- main.go
+++ main.go
@@ -2,39 +2,38 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"syscall"
+	"net/http"
 
-	"github.com/fvbock/endless"
 	"github.com/qmz/go-gin-blog/pkg/setting"
 	"github.com/qmz/go-gin-blog/routers"
 )
 
 func main() {
 
-	// routersInit := routers.InitRouter()
+	routersInit := routers.InitRouter()
 
-	// s := &http.Server{
-	// 	Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-	// 	Handler:        routersInit,
-	// 	ReadTimeout:    setting.ReadTimeout,
-	// 	WriteTimeout:   setting.WriteTimeout,
-	// 	MaxHeaderBytes: 1 << 20,
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        routersInit,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
+
+	// endless.DefaultReadTimeOut = setting.ReadTimeout
+	// endless.DefaultWriteTimeOut = setting.WriteTimeout
+	// endless.DefaultMaxHeaderBytes = 1 << 20
+	// endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+
+	// server := endless.NewServer(endPoint, routers.InitRouter())
+	// server.BeforeBegin = func(add string) {
+	// 	log.Printf("Actual pid is %d", syscall.Getpid())
 	// }
 
-	// s.ListenAndServe()
-	endless.DefaultReadTimeOut = setting.ReadTimeout
-	endless.DefaultWriteTimeOut = setting.WriteTimeout
-	endless.DefaultMaxHeaderBytes = 1 << 20
-	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
-
-	server := endless.NewServer(endPoint, routers.InitRouter())
-	server.BeforeBegin = func(add string) {
-		log.Printf("Actual pid is %d", syscall.Getpid())
-	}
-
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Printf("Server err: %v", err)
-	}
+	// err := server.ListenAndServe()
+	// if err != nil {
+	// 	log.Printf("Server err: %v", err)
+	// }
 }
--- routers/router.go
+++ routers/router.go
@@ -1,45 +1,48 @@
 package routers
 
 import (
-	"github.com/qmz/go-gin-blog/middleware/jwt"
 	"github.com/gin-gonic/gin"
+	_ "github.com/qmz/go-gin-blog/docs"
+	"github.com/qmz/go-gin-blog/middleware/jwt"
 	"github.com/qmz/go-gin-blog/pkg/setting"
-	"github.com/qmz/go-gin-blog/routers/api/v1"
+	v1 "github.com/qmz/go-gin-blog/routers/api/v1"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 func InitRouter() *gin.Engine {
-    r := gin.New()
+	r := gin.New()
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.Use(gin.Logger())
 
-    r.Use(gin.Logger())
+	r.Use(gin.Recovery())
 
-    r.Use(gin.Recovery())
+	gin.SetMode(setting.RunMode)
+	r.GET("/auth", v1.GetAuth)
 
-    gin.SetMode(setting.RunMode)
-    r.GET("/auth", v1.GetAuth)
+	apiv1 := r.Group("/api/v1")
 
-    apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
+	{
+		//获取标签列表
+		apiv1.GET("/tags", v1.GetTags)
+		//新建标签
+		apiv1.POST("/tags", v1.AddTag)
+		//更新指定标签
+		apiv1.PUT("/tags/:id", v1.EditTag)
+		//删除指定标签
+		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+		//获取文章列表
+		apiv1.GET("/articles", v1.GetArticles)
+		//获取指定文章
+		apiv1.GET("/articles/:id", v1.GetArticle)
+		//新建文章
+		apiv1.POST("/articles", v1.AddArticle)
+		//更新指定文章
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		//删除指定文章
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
+	}
 
-    apiv1.Use(jwt.JWT())
-    {
-        //获取标签列表
-        apiv1.GET("/tags", v1.GetTags)
-        //新建标签
-        apiv1.POST("/tags", v1.AddTag)
-        //更新指定标签
-        apiv1.PUT("/tags/:id", v1.EditTag)
-        //删除指定标签
-        apiv1.DELETE("/tags/:id", v1.DeleteTag)
-        //获取文章列表
-        apiv1.GET("/articles", v1.GetArticles)
-        //获取指定文章
-        apiv1.GET("/articles/:id", v1.GetArticle)
-        //新建文章
-        apiv1.POST("/articles", v1.AddArticle)
-        //更新指定文章
-        apiv1.PUT("/articles/:id", v1.EditArticle)
-        //删除指定文章
-        apiv1.DELETE("/articles/:id", v1.DeleteArticle)
-    }
-
-    return r
+	return r
 }
